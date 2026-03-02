4f9430263fd77dbeaf1e5045bdf41cb8cefb0581
Mike Koh <mike.koh@onwardrides.com>
2026-03-02 10:29:55 -0800

    Configure database connection and fix debugger extension id

--- .devcontainer/rails/post_create_command.sh
+++ .devcontainer/rails/post_create_command.sh
@@ -9,18 +9,67 @@ sudo chmod +x /workspaces/$PROJECT_NAME/rails/trading/bin/*
 
 mkdir -p $HOME/.dbclient/storage
 cat > $HOME/.dbclient/storage/config.jsonc << 'EOF'
+// This file is a temporary file, the configuration is not actually stored here.
 {
   "database": {
     "1000000000000": {
-      "key": "1000000000000",
-      "isTreeNode": true,
+      "host": "127.0.0.1",
+      "port": 3306,
       "dbType": "SQLite",
-      "database": "/workspaces/eng-tech-tasks/rails/trading/db/development.sqlite3",
       "name": "Development",
+      "advance": {
+        "idleConfig": {
+          "enable": true
+        },
+        "hideSystemSchema": true,
+        "groupingTables": false,
+        "loadMetaDataWhenExpandTreeView": true
+      },
+      "treeFeatures": [],
+      "usingSSH": false,
+      "useSocksProxy": false,
+      "useHTTPProxy": false,
       "global": true,
       "savePassword": "Forever",
       "readonly": false,
       "sort": 1,
+      "useSSL": false,
+      "dbPath": "/workspaces/eng-tech-tasks/rails/trading/db/development.sqlite3",
+      "fs": {
+        "encoding": "utf8",
+        "showHidden": true
+      },
+      "key": "1000000000000",
+      "connectionKey": "database.connections"
+    },
+    "1000000000001": {
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dbType": "SQLite",
+      "name": "Test",
+      "advance": {
+        "idleConfig": {
+          "enable": true
+        },
+        "hideSystemSchema": true,
+        "groupingTables": false,
+        "loadMetaDataWhenExpandTreeView": true
+      },
+      "treeFeatures": [],
+      "usingSSH": false,
+      "useSocksProxy": false,
+      "useHTTPProxy": false,
+      "global": true,
+      "savePassword": "Forever",
+      "readonly": false,
+      "sort": 2,
+      "useSSL": false,
+      "dbPath": "/workspaces/eng-tech-tasks/rails/trading/db/test.sqlite3",
+      "fs": {
+        "encoding": "utf8",
+        "showHidden": true
+      },
+      "key": "1000000000001",
       "connectionKey": "database.connections"
     }
   },
