afd332f81d5dc62d09b4d0d4913b6dfdb11d42f6
Mike Koh <mike.koh@onwardrides.com>
2026-03-02 10:12:38 -0800

    Move configs to workspace configs

--- .devcontainer/rails/post_create_command.sh
+++ .devcontainer/rails/post_create_command.sh
@@ -7,6 +7,28 @@ PROJECT_NAME=$(basename "$(pwd)")
 sudo chown -R vscode:vscode /workspaces/$PROJECT_NAME
 sudo chmod +x /workspaces/$PROJECT_NAME/rails/trading/bin/*
 
+mkdir -p $HOME/.dbclient/storage
+cat > $HOME/.dbclient/storage/config.jsonc << 'EOF'
+{
+  "database": {
+    "1000000000000": {
+      "key": "1000000000000",
+      "isTreeNode": true,
+      "dbType": "SQLite",
+      "database": "/workspaces/eng-tech-tasks/rails/trading/db/development.sqlite3",
+      "name": "Development",
+      "global": true,
+      "savePassword": "Forever",
+      "readonly": false,
+      "sort": 1,
+      "connectionKey": "database.connections"
+    }
+  },
+  "nosql": null,
+  "$schema": "https://cdn.database-client.com/dbclient/schema.json"
+}
+EOF
+
 cd rails/trading
 rails db:create
 rails db:migrate
