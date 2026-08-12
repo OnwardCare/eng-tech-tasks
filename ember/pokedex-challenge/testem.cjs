fbc1446a3a31c51e42dbc5f8af85723004620fd2
Arielitobc <arielitobc@gmail.com>
2026-08-12 18:20:24 -0400

    unit test

--- ember/pokedex-challenge/testem.cjs
+++ ember/pokedex-challenge/testem.cjs
@@ -6,15 +6,18 @@ if (typeof module !== 'undefined') {
     disable_watching: true,
     launch_in_ci: ['Chrome'],
     launch_in_dev: ['Chrome'],
-    browser_start_timeout: 120,
+    browser_start_timeout: 180,
     browser_args: {
       Chrome: {
         ci: [
           // --no-sandbox is needed when running Chrome inside a container
           process.env.CI ? '--no-sandbox' : null,
-          '--headless',
+          // The new headless mode is more stable on recent Chrome versions
+          // than the legacy '--headless' flag.
+          '--headless=new',
           '--disable-dev-shm-usage',
           '--disable-software-rasterizer',
+          '--disable-gpu',
           '--mute-audio',
           '--remote-debugging-port=0',
           '--window-size=1440,900',
